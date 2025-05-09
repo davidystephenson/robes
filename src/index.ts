1056fe6e92bb4331474d34ee2494ae04ab91d509
David Y. Stephenson <david@davidystephenson.com>
2025-05-09 00:49:09 -0400

    Expose context

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import creation from './robesContext'
 export { default as ButtonRobe } from './ButtonRobe'
 export { default as IconButtonRobe } from './IconButtonRobe'
 export { default as IconImpressedRobe } from './IconImpressedRobe'
@@ -14,3 +15,4 @@ export { default as RowmanceTableRobe } from './RowmanceTableRobe'
 export { default as SwitchRobe } from './SwitchRobe'
 export { default as TableRobe } from './TableRobe'
 export type { InputRobeProps, LayoutRobeProps, LinkRobeProps, NavbarRobeProps, ProfileRobeProps } from './types'
+export const { context: robesContext } = creation
