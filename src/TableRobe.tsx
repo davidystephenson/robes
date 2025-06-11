1666ed4b702c19df84c07cf54a95e19cf6413488
David Y. Stephenson <david@davidystephenson.com>
2025-06-11 16:02:40 -0400

    Remove Th prop

--- src/TableRobe.tsx
+++ src/TableRobe.tsx
@@ -1,9 +1,8 @@
 import { Table, TableProps } from '@chakra-ui/react'
 import { JSX } from 'react'
-import WideFirstColumnRobe from './WideFirstColumnRobe'
 
 export default function TableRobe (props: TableProps): JSX.Element {
   return (
-    <Table size='sm' Th={WideFirstColumnRobe} {...props} />
+    <Table size='sm' {...props} />
   )
 }
